df84982395f7d7268f2024140b8989d8fe1c4898
lincaiming <lincaiming5837@gmail.com>
2019-04-12 21:02:55 +0800

    upate .gitignore

--- tools/demo.py
+++ tools/demo.py
@@ -101,12 +101,12 @@ def parse_args():
     """Parse input arguments."""
     parser = argparse.ArgumentParser(description='Faster R-CNN demo')
     parser.add_argument('--gpu', dest='gpu_id', help='GPU device id to use [0]',
-                        default=1, type=int)
+                        default=0, type=int)
     parser.add_argument('--cpu', dest='cpu_mode',
                         help='Use CPU mode (overrides --gpu)',
                         action='store_true')
-    parser.add_argument('--net', dest='demo_net', help='Network to use [vgg16]',
-                        choices=NETS.keys(), default='vgg16')
+    parser.add_argument('--net', dest='demo_net', help='Network to use [zf]',
+                        choices=NETS.keys(), default='zf')
 
     args = parser.parse_args()
 
